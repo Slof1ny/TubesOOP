c5f544ca36d9ad2bbaf1150e2b6d6d39948c85c7
nathan_priandi <18223032@std.stei.itb.ac.id>
2025-05-07 18:34:31 +0700

    update tile and fishinglocation classes

--- src/core/world/Tile.java
+++ src/core/world/Tile.java
@@ -3,33 +3,83 @@ package core.world;
 import item.Crop;
 
 public class Tile {
-    enum TileType {
-        UNTILLED,  
-        TILLED,   
-        PLANTED,
-        DEPLOYED   
+    public enum TileType {
+        UNTILLED,   
+        TILLED,     
+        PLANTED,   
+        DEPLOYED  
     }
     
     private final int x, y;
     private TileType type;
+    private char deployedChar;
     private Crop plantedCrop;
-    private FarmMap map;
 
-    public Tile(int x, int y) throws IllegalArgumentException { 
-        if (x < 0 || y < 0) {
-            throw new IllegalArgumentException("Coordinates must be non-negative.");
+    public Tile(int x, int y) {
+        if (x < 0 || y < 0 || x >= FarmMap.SIZE || y >= FarmMap.SIZE) {
+            throw new IllegalArgumentException("Coordinates out of bounds");
         }
-        else if (map.grid[x][y].type == TileType.DEPLOYED) {
-            throw new IllegalArgumentException("Tile already exists at this location.");
-        }
-        
-        this.x = x; 
+        this.x = x;
         this.y = y;
         this.type = TileType.UNTILLED;
         this.plantedCrop = null;
+        this.deployedChar = ' ';
+    }
+
+    public int getX() { 
+        return x; 
+    }
+
+    public int getY() { 
+        return y; 
+    }
+
+    public TileType getType() { 
+        return type; 
+    }
+
+    public Crop getPlantedCrop() { 
+        return plantedCrop; 
+    }
+
+    public void setType(TileType type) {
+        if (type == TileType.DEPLOYED) {
+            throw new IllegalStateException("Use deployObject method to mark a tile as DEPLOYED");
+        }
+        this.type = type;
+        this.deployedChar = ' ';
+        if (type != TileType.PLANTED) {
+            plantedCrop = null;
+        }
+    }
+    
+    public void deployObject(char c) {
+        if (c == 'h' || c == 'o' || c == 's') {
+            this.type = TileType.DEPLOYED;
+            this.deployedChar = c;
+        }
+        else {
+            throw new IllegalArgumentException("Invalid deployed object character: " + c);
+        }
+        
+    }
+
+    public void clearDeployment() {
+        this.type = TileType.UNTILLED;
+        this.deployedChar = ' ';
     }
 
     public boolean isWalkable() {
         return type != TileType.DEPLOYED;
+    }
+
+    public char displayChar() {
+        switch (type) {
+            case UNTILLED:  return '.';
+            case TILLED:    return 't';
+            case PLANTED:   return 'l';
+            case DEPLOYED:  return deployedChar;
+        }
+        return ' ';
     }
 }
--- src/fishing/SpecificFishingLocation.java
+++ src/fishing/SpecificFishingLocation.java
@@ -1,29 +1,54 @@
 package fishing;
 
 import java.util.List;
+import java.util.ArrayList;
+
 import item.Fish;
 import core.world.*;
-import core.player.Player;
+import core.world.Tile.*;
+import core.player.*;
 
 public class SpecificFishingLocation extends FishingLocation {
-    protected FarmMap farmMap;
-    protected List<Tile> fishingPositions; // nanti cari posisi pond dlu
+    private static final int[][] ADJACENT = {
+        {0, 1}, {0, -1}, {1, 0}, {-1, 0}
+    };
 
-    public SpecificFishingLocation(String name, List<Fish> possibleFish, FarmMap farmMap, List<Tile> fishingPositions) {
+    protected FarmMap farmMap;
+    protected List<Tile> fishingPositions;
+
+    public SpecificFishingLocation(String name, List<Fish> possibleFish, FarmMap farmMap) {
         super(name, possibleFish);
         this.farmMap = farmMap;
-        this.fishingPositions = fishingPositions; // loop matriks buat cari posisi pond baru +1
+        this.fishingPositions = new ArrayList<>();
+        getFishingPositions();
+    }
+
+    private void getFishingPositions() {
+        for (int x = 0; x < FarmMap.SIZE; x++) {
+            for (int y = 0; y < FarmMap.SIZE; y++) {
+                Tile t = farmMap.getTileAt(x, y);
+                if (t.getType() == TileType.DEPLOYED && t.displayChar() == 'o') {
+                    for (int[] d : ADJACENT) {
+                        int nx = x + d[0], ny = y + d[1];
+                        if (nx >= 0 && ny >= 0 && nx < FarmMap.SIZE && ny < FarmMap.SIZE) {
+                            Tile adj = farmMap.getTileAt(nx, ny);
+                            if (adj.isWalkable()) {
+                                fishingPositions.add(adj);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     @Override
     public boolean canFishAt(Player player) {
-        if (player.getLocation().equals("Farm Map")) {
-            for (Tile tile : fishingPositions) {
-                if (tile.equals(player.getCurrentTile())) {
-                    return true;
-                }
-            }
+        if (!player.getLocation().equals("Farm Map")) {
+            return false;
         }
-        return false;
+
+        Tile current = player.getCurrentTile();
+        return fishingPositions.contains(current);
     }
 } 

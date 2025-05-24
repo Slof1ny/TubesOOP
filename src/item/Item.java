0c5947afe661c97950c5e1c5f41124c6ae2eeeab
Slof1ny <hugatama@gmail.com>
2025-05-24 14:44:08 +0700

    feat: add basic inventory

--- src/item/Item.java
+++ src/item/Item.java
@@ -24,5 +24,18 @@ public abstract class Item {
     }
 
     public abstract String getCategory();
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Item item = (Item) obj;
+        return name.equals(item.name);
+    }
+    
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
     
 }
